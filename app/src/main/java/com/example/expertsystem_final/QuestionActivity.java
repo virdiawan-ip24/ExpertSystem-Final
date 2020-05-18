5f28abee10f988aaf2375ecb2d510b055024bfc2
Virdiawan IP <dvirdiawan@it.student.pens.ac.id>
2020-05-18 19:54:09 +0700

    Commit 18/05/2020 - membuat tampilan main menu

--- app/src/main/java/com/example/expertsystem_final/QuestionActivity.java
+++ app/src/main/java/com/example/expertsystem_final/QuestionActivity.java
@@ -3,12 +3,18 @@ package com.example.expertsystem_final;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class QuestionActivity extends AppCompatActivity {
+public class QuestionActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
