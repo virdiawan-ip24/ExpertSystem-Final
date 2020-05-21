57147c8a76a306ee5a9ff82082b2b0f121da2743
Virdiawan IP <dvirdiawan@it.student.pens.ac.id>
2020-05-21 22:35:12 +0700

    Commit 21/05/2020 - membuat activity pertanyaan

--- app/src/main/java/com/example/expertsystem_final/Question/QuestionActivity.java
+++ app/src/main/java/com/example/expertsystem_final/Question/QuestionActivity.java
@@ -4,15 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
+import com.example.expertsystem_final.DBConfig.DBConfigKondisiActivity;
+import com.example.expertsystem_final.DBConfig.DBConfigQuestionActivity;
 import com.example.expertsystem_final.R;
 
 public class QuestionActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView textQuestion;
+    private RadioGroup radioKondisi;
+    private RadioButton radioButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question);
+
+        textQuestion = (TextView) findViewById(R.id.textQuestion);
+        
+
+
     }
 
     @Override
